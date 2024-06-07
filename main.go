ee500c200a03d2569fdd8c4faf224c48e19053fa
Adam Rao <adamokoth@protonmail.com>
2024-06-07 14:27:24 +0300

    chore: integrate postgres to api

--- main.go
+++ main.go
@@ -1,55 +1,116 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
+	"fmt"
 	"net/http"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 )
 
-type album struct {
-    ID     string  `json:"id"`
-    Title  string  `json:"title"`
-    Artist string  `json:"artist"`
-    Price  float64 `json:"price"`
+type Album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
 }
 
-var albums = []album{
-    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+func dbFactory() (*sql.DB, error) {
+	DB_NAME := os.Getenv("DB_NAME")
+	DB_USER := os.Getenv("DB_USER")
+	DB_PASS := os.Getenv("DB_PASS")
+
+	connStr := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PASS, DB_NAME)
+
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		return db, errors.New("could not connect to database")
+	}
+
+	return db, nil
 }
 
 func getAlbums(c *gin.Context) {
-    c.IndentedJSON(http.StatusOK, albums)
-}
+	var albums []Album
 
-func postAlbums(c *gin.Context) {
-	var newAlbum album
+	db, err := dbFactory()
 
-	if err := c.BindJSON(&newAlbum); err != nil {
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"statusCode": http.StatusInternalServerError, "error": err.Error()})
 		return
 	}
 
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
+	defer db.Close()
+
+	cur, err := db.Query("SELECT * FROM album")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer cur.Close()
+
+	for cur.Next() {
+		var album Album
+
+		if err := cur.Scan(&album.ID, &album.Title, &album.Artist, &album.Price); err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, gin.H{"statusCode": http.StatusInternalServerError, "error": "something went wrong"})
+		}
+
+		albums = append(albums, album)
+	}
+
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbums(c *gin.Context) {
+	var newAlbum Album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"statusCode": http.StatusBadRequest, "error": "Could not add album. Try again"})
+		return
+	}
+
+	db, err := dbFactory()
+
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	defer db.Close()
+
+	_, err = db.Exec("INSERT INTO album (title, artist, price) VALUES ($1, $2, $3)", newAlbum.Title, newAlbum.Artist, newAlbum.Price)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"statusCode": http.StatusBadRequest, "error": err.Error()})
+		return
+	}
+
+	c.IndentedJSON(http.StatusCreated, gin.H{"statusCode": http.StatusCreated, "message": "Album created successfully"})
 }
 
 func getAlbumByID(c *gin.Context) {
-    id := c.Param("id")
+	// id := c.Param("id")
 
-    for _, a := range albums {
-        if a.ID == id {
-            c.IndentedJSON(http.StatusOK, a)
-            return
-        }
-    }
-    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	// for _, a := range albums {
+	//     if a.ID == id {
+	//         c.IndentedJSON(http.StatusOK, a)
+	//         return
+	//     }
+	// }
+	c.IndentedJSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "album not found"})
 }
 
 func main() {
-    router := gin.Default()
-    router.GET("/albums", getAlbums)
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
 	router.POST("/albums", postAlbums)
 	router.GET("/albums/:id", getAlbumByID)
 
-    router.Run("localhost:8080")
+	router.Run("localhost:8080")
 }
